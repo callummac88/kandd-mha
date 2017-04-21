a2afaaaa4875942ced2da2ebaad5229fb9998137
Callum Macdonald <callum@redevolution.com>
2017-04-21 17:35:37 +0100

    updated flow flow plugin
    Signed-off-by: Callum Macdonald <callum@redevolution.com>

---
+++ admin/plugins/flow-flow/ff-config.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Flow-Flow
+ *
+ * Plugin class. This class should ideally be used to work with the
+ * public-facing side of the WordPress site.
+ *
+ * If you're interested in introducing administrative or dashboard
+ * functionality, then refer to `FlowFlowAdmin.php`
+ *
+ * @package   FlowFlow
+ * @author    Looks Awesome <email@looks-awesome.com>
+
+ * @link      http://looks-awesome.com
+ * @copyright 2014-2016 Looks Awesome
+ */
+define('FF_PLUGIN_URL', 'http://local-kdmha.com/admin/plugins');
+define('FF_AJAX_URL',   'http://local-kdmha.com/admin/plugins/flow-flow/ff.php');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME','kanddmha');//DB NAME
+define('DB_USER', 'root');//DB USER NAME
+define('DB_PASSWORD', 'root');//DB PASSWORD
+
+define('DB_CHARSET','utf8');
+define('DB_COLLATE','utf8_general_ci');
+define('DB_TABLE_PREFIX', '');//TABLE PREFIX
+
+define('FF_LOCALE', 'en_US');
+//date_default_timezone_set('America/New_York');
